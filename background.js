cd514c7623647f69926736f8fe6ef83e01c205fb
Den_dp <Denis.Bendrikov@gmail.com>
2014-09-14 11:35:33 +0300

    fixed broken url

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // React when a browser action's icon is clicked.
 chrome.browserAction.onClicked.addListener(function(currentTab) {
-    var apiUrl = "http://summarly-api.herokuapp.com/summary?";
+    var apiUrl = "http://summarly.herokuapp.com/summary?";
     var params = "url=" + encodeURI(currentTab.url);
     chrome.tabs.create({url: apiUrl + params});
 });
